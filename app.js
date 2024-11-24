6a654180cfc6136b56fe5a0251192a21b84e5590
Sicktooth <sicktooth003@gmail.com>
2024-11-25 00:53:00 +0100

    install passportJS, passport-local-mongoose, express-session & passport-local(dependency for passport-local-mongoose) and use of passportJs for authentication(registration and login) & store cookies using express-session that ends after a browser session to enable user navigate in and out of secrets route and still be authenticated in browser session

--- app.js
+++ app.js
@@ -1,84 +1,79 @@
 const express = require("express");
-const PORT = 3000;
+const session = require('express-session');
+const passport = require('passport');
+const { connectDB, User } = require('./config/database');
+
 const app = express();
+const PORT = 3000;
+
+// Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-const {connectDB, User} = require('./config/database')
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
 
-connectDB();
-app.get('/', (req, res)=>{
-    res.render('home');
-})
+app.use(session({
+    secret: "ourLittleSecret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { // this was required compared to the course
+        secure: false, // Use true with HTTPS
+        httpOnly: true,
+        expires: undefined, // Session expires when the browser is closed
+    },
+}));
 
-app.get('/login', (req, res)=>{
-    res.render('login');
-})
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/register', (req, res)=>{
-    res.render('register');
-})
+// Connect to database
+connectDB().then(() => console.log("Connected to MongoDB")).catch(console.error);
 
-app.post('/register', async (req, res)=> {
-    const userName = req.body.username,
-    password = req.body.password
-    const hash = bcrypt.hashSync(password, saltRounds);
-    try {
-        const checkUser = await User.findOne({email: userName});
-        if (!checkUser) {
-            const newUser = await User.create({
-                email: userName,
-                password: hash
-            });
-            if (newUser){
-                console.log("Successfully add new user");
-                res.render('secrets')
-            } 
-        } else {
-            res.send({message: "This email has been used"});
-        }
-        
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({
-            message: "internal server error",
-            error: error.message, error
-        })
+// Passport Configuration
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/login', (req, res) => res.render('login'));
+app.get('/register', (req, res) => res.render('register'));
+app.get('/logout', (req, res)=>
+    req.logout(function(err) {
+        if (err) { return next(err); }
+        res.redirect('/');
+      })) // this required a callback function compared to the course
+
+// Protected route
+app.get('/secrets', (req, res) => {
+    if (req.isAuthenticated()) { //isAunthenticated is a function compared to the course
+        res.render('secrets');
+    } else {
+        res.redirect('/login');
     }
-   
-})
+});
 
-app.post("/login", async (req, res)=>{
-    const userName = req.body.username,
-    password = req.body.password;
-    
-    try {
-        const foundUser = await User.findOne({email: userName});
-        if(foundUser){
-            if(bcrypt.compareSync(password, foundUser.password)){
-                res.render('secrets')
-            } else {
-                res.send({
-                    message: 'incorrect password'
-                });
+// Register user
+app.post('/register', (req, res) => {
+    User.register({ username: req.body.username }, req.body.password, (err, user) => {
+        if (err) {
+            console.error(err);
+            return res.status(400).render('register', { message: "Registration failed", error: err.message });
+        }
+        req.login(user, (err) => { // this is required to login the user after registration to the database compared to passport.authenticate in the course
+            if (err) {
+                console.error(err);
+                return res.status(500).render('register', { message: "Login after registration failed", error: err.message });
             }
-        } else {
-            res.status(404).send({
-                message: "User not found please register"
-            })
-        }
-    } catch (error) {
-        console.log(error)
-        res.status(500).send({
-            message: "internal server error",
-            error: error.message, error
-        })
-    }
-})
+            res.redirect('/secrets');
+        });
+    });
+});
 
-app.listen(PORT, ()=>{
-    console.log('server started at localhost:'+PORT);
-    
-})
+// Login user
+app.post('/login', passport.authenticate('local', { // this changed too and it became simpler as seen compared to the course that used passport.authenticate
+    successRedirect: '/secrets',
+    failureRedirect: '/login',
+}));
+
+// Start server
+app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
--- config/database.js
+++ config/database.js
@@ -1,16 +1,15 @@
 // require('dotenv').config();
 const mongoose = require('mongoose');
 const {Schema} = mongoose;
+const passportLocalMongoose = require('passport-local-mongoose')
 
 const connectDB = ()=> mongoose.connect('mongodb://localhost:27017/userDB');
 const userSchema = new Schema({
-    email: {type: String, required: true},
-    password: {type: String, required: true}
+    email: String,
+    password: String
   })
+userSchema.plugin(passportLocalMongoose);
 const User = mongoose.model('User', userSchema)
 
 
-module.exports = {
-    User,
-    connectDB
-}
+module.exports = {User, connectDB}
